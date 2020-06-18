5ec3125bf7fdc60259ab621983efc82d8bd6e62c
Ketki Chiplunkar <chiplunkar.k1@gmail.com>
2020-06-18 15:51:41 +0530

    This is a test commit

---
+++ src/DemoPackage/DemoTestClass.java
@@ -0,0 +1,10 @@
+package DemoPackage;
+
+public class DemoTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("This git demo");
+	}
+
+}
